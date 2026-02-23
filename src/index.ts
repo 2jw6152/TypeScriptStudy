aea6f1490f952df8d2b79a711d22dc05401106c0
2jw6152 <2jw6152@gmail.com>
2026-02-23 15:32:03 +0900

    놀라운 발견

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-const message: string = "Hello, TypeScript!";
+let i = 'a';
+const obj = {
+    name: "Joonwoo",
+    [i]: 21
+}
 
-console.log(message);
+while (i<'d'){
+    console.log(obj.a);
+    i = String.fromCharCode(i.charCodeAt(0) + 1);
+}
+
+// 21
+// undefined
+// 21
+// undefined
+// 21
+// undefined
